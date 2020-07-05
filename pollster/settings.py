36b1aca3acac16016b26eec06a4d1b33825571af
Soojung Kwak <gsj2464@gmail.com>
2020-07-05 15:40:38 -0400

    completed application

--- pollster/settings.py
+++ pollster/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't5t%!gk!d5!t8okg9+u=%556ii91y!!&zn!shb!56vs^v-b25e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
